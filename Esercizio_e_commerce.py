ef95290f31848a444a213af2a3c65a13024f5433
DennisXIII <dennisesposito.business@gmail.com>
2025-05-21 20:34:50 +0200

    update file
    update file

---
+++ Esercizio_e_commerce.py
@@ -0,0 +1,158 @@
+import mysql.connector
+
+def esegui_query_insert(query, valori):
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="ecommerce"
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute(query, valori)
+    conn.commit()
+
+    print(f"{cursor.rowcount} record inserito.")
+
+    cursor.close()
+    conn.close()
+
+def query_select(query, valori):
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="ecommerce"
+    )
+
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute(query, valori)
+    risultati = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return risultati
+
+
+def login_utente():
+    nome_utente = input("Inserisci nome utente: ")
+    password = input("Inserisci password: ")
+
+    q = "SELECT * from utenti where username = %s and pswd = %s"
+    v = (nome_utente, password)
+
+    risultato = query_select(q, v)
+
+    # print(risultato)
+
+    if len(risultato) == 0:
+        print("Utente non esistente o password errata")
+        return -1
+    else:
+        print(f"Benvenuto {risultato[0]['username']}")
+        print(f"Hai accesso come {risultato[0]['ruolo']}")
+
+        return risultato[0]
+
+def menu_cliente():
+    print("1) Visualizza saldo")
+    print("2) Ricarica credito")
+    print("3) Visualizza prodotti")
+    print("4) Acquista prodotto")
+    print("5) Visualizza storico acquisti")
+    print("6) Logout")
+
+
+def menu_start():
+    print("1) Registra utente cliente")
+    print("2) Registra utente admin")
+    print("3) Login utente")
+    print("4) Esci")
+
+def menu_admin():
+    print("1) Visualizza saldo")
+    print("2) Aggiungi nuovo prodotto")
+    print("3) Visualizza prodotti")
+    print("4) Rimuovi prodotto")
+    print("5) Visualizza storico di tutti gli utenti")
+    print("6) Logout")
+
+def registra_utente_cliente():
+    nome_utente = input("Inserisci nome utente: ")
+    password = input("Inserisci password: ")
+
+    q = "insert into utenti(username, pswd, ruolo) VALUES (%s, %s, %s);"
+    v = (nome_utente, password, "cliente")
+
+    esegui_query_insert(q, v)
+def registra_utente_admin():
+    nome_utente = input("Inserisci nome utente: ")
+    password = input("Inserisci password: ")
+
+    q = "insert into utenti(username, pswd, ruolo) VALUES (%s, %s, %s);"
+    v = (nome_utente, password, "admin")
+
+    esegui_query_insert(q, v)
+
+
+def visualizza_saldo(id_utente):
+    q = "select credito from utenti where id = %s"
+    v = (id_utente,)
+
+    ris = query_select(q, v)
+
+    print(f"Il tuo credito è: {ris[0]['credito']}")
+
+def visualizza_prodotti_disponibili(id_prodotto):
+    q = "select nome from prodotti where id = %s"
+    v = (id_prodotto,)
+
+    ris = query_select(q, v)
+
+    print(f"I prodotti sono è: {ris[0]['nome']}")
+
+
+
+if __name__ == '__main__':
+    while True:
+        menu_start()
+        scelta_start = int(input("Scelta: "))
+
+        if scelta_start == 1:
+            registra_utente_cliente()
+        elif scelta_start == 2:
+            registra_utente_admin()
+        elif scelta_start == 3:
+            risultato = login_utente()
+
+            if risultato != -1:
+                if risultato['ruolo'] == "admin":
+                    menu_admin()
+                else:
+                    id_utente = risultato['id_utente']
+                    while True:
+                        menu_cliente()
+
+                        scelta_cliente = int(input("Scelta: "))
+
+                        if scelta_cliente == 1:
+                            visualizza_saldo(id_utente)
+                        elif scelta_cliente == 2:
+                            ricarica_credito()
+                        elif scelta_cliente == 3:
+                            visualizza_prodotti_disponibili(id_prodotto)
+                        elif scelta_cliente == 4:
+                            ricarica_credito()
+                        elif scelta_cliente == 5:
+                            ricarica_credito()
+                        elif scelta_cliente == 6:
+                            break
+
+
+        elif scelta_start == 4:
+            break
+
+
